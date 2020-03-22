47591893ac7770610a05d1d2dea1ac717b1839f1
Adrian Foong <adrian_foong@hotmail.com>
2020-03-21 23:09:06 -0600

    update func

--- think/index.js
+++ think/index.js
@@ -1,15 +1,30 @@
-module.exports = async function(context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+// module.exports = async function(context, req) {
+//   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const state = req.body;
+//   const state = req.body;
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
+//   context.res = {
+//     // status: 200, /* Defaults to 200 */
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*"
+//     },
+//     body: { position: state.cells.findIndex(cell => !cell.mark) }
+//   };
+//   context.done();
+// };
+
+exports.handler = async function(event, context) {
+  const state = event.body;
+
+  const response = {
+    statusCode: "200",
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
     },
     body: { position: state.cells.findIndex(cell => !cell.mark) }
   };
-  context.done();
+
+  return response;
 };
